fb047cc6746255c36209d4d68cfe5a4f7b48a530
Christian Llupo <jebediah47@protonmail.com>
2022-06-21 12:12:34 +0300

    Update Downloads.svelte

--- src/pages/Downloads.svelte
+++ src/pages/Downloads.svelte
@@ -1,4 +1,4 @@
-<div class="bg-slate-800" id="downloads__component">
+<div class="bg-slate-800" id="downloads__element">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-7">
     <div class="lg:text-center md:text-center sm:text-center">
       <h2 class="text-indigo-600 font-semibold tracking-wide uppercase text-lg">
@@ -18,25 +18,37 @@
   <div class="flex flex-wrap justify-center items-center py-10">
     <div class="card w-96 bg-base-100 shadow-xl">
       <figure>
-        <img src="https://i.imgur.com/Giefeo3.jpeg" alt="Screenshot" />
+        <img
+          src="https://i.imgur.com/Giefeo3.jpeg"
+          alt="Screenshot XFCE edition"
+        />
       </figure>
       <div class="card-body">
         <h2 class="card-title">Woof OS XFCE edition.</h2>
         <p>XFCE flavour of Woof OS, more beginner friendly.</p>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary">Download now</button>
+          <a
+            href="https://github.com/woof-os/isos-xfce4/releases/latest/download/woofos.iso"
+            class="btn btn-primary">Download now</a
+          >
         </div>
       </div>
     </div>
     <div class="card w-96 bg-base-100 shadow-xl lg:ml-96">
       <figure>
-        <img src="https://i.imgur.com/BM9GQqT.png" alt="Screenshot" />
+        <img
+          src="https://i.imgur.com/BM9GQqT.png"
+          alt="Screenshot Qtile Edition"
+        />
       </figure>
       <div class="card-body">
         <h2 class="card-title">Woof OS Qtile edition.</h2>
         <p>Qtile edition Woof OS, recommended for power users</p>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary">Download now</button>
+          <a
+            href="https://github.com/woof-os/isos-qtile/releases/latest/download/woofos.iso"
+            class="btn btn-primary">Download now</a
+          >
         </div>
       </div>
     </div>
